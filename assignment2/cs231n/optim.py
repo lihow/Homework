0fa155441fa6d6d4469097e417cef6675cb2f6ff
lihow <2458858175@qq.com>
2018-05-24 10:37:51 +0800

    finish FC

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -100,7 +100,10 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    cache = config.get('cache')
+    cache = config['decay_rate'] * cache + (1 - config['decay_rate']) * dx ** 2
+    next_x = x - config['learning_rate'] * dx / (np.sqrt(cache) + config['epsilon'])
+    config['cache'] = cache
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -137,7 +140,15 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    m = config.get('m')
+    m = config['beta1'] * m + (1 - config['beta1']) * dx
+    mt = m / (1 - config['beta1'] ** config['t'])
+    v = config.get('v')
+    v = config['beta2'] * v + (1 - config['beta2']) * (dx ** 2)
+    vt = v / (1 - config['beta2'] ** config['t'])
+    next_x = x - config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
+    config['m'] = m
+    config['v'] = v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
